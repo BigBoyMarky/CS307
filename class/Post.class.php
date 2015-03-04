10d7bb18356b39de71b48e5ba1a0815b8a9c49b8
tan126 <tan126@purdue.edu>
2015-03-03 20:31:22 -0500

    Modified Post class

--- class/Post.class.php
+++ class/Post.class.php
@@ -1,99 +1,75 @@
 <?php
-
 /*
-*	Author: Aaron & Kai Jun
-*	Date: 2/28/2015
-*	Time: 5:30 pm
+* Author: Aaron & Kai Jun
+* Date: 2/28/2015
+* Time: 5:30 pm
 */
 
+/*
+ *  Last modified : 3/3/2015
+ *  1) Removed all utility function to the PostsHandler class
+ *  2) Modified variables name to match database columns.
+ *  3) Modified create function so it will support updating depending on parameter
+ *  4) Temporarily removed location entry. Will be added in the future as a feature.
+ *
+ *  -- jay
+ */
 
 require_once "DB.class.php";
 require_once "User.class.php";
 
 class Post{
 
-	public $postid;
-	public $user;
+    public $postid;
+    public $userID;
+    public $fname;
+    public $lname;
+    public $age;
+    public $gender;
+    //public $location;
+    public $contactNumber;
+    public $emailAddress;
+    public $additionalInfo;
 
-	public $firstName;
-	public $lastName;
-	public $age;
-	public $gender;
-	public $location;
-	public $contactNumber;
-	public $emailAddress;
-	public $additionalInfo;
-
-	 function __construct($data) {
-        $this->user = isset($data['user']) ? $data['user'] : "";
-        $this->firstName = isset($data['firstName']) ? $data['firstName'] : "";
-        $this->lastName = isset($data['lastName']) ? $data['lastName'] : "";
+    function __construct($data) {
+        $this->userID = isset($data['userID']) ? $data['userID'] : "";
+        $this->fname = isset($data['fname']) ? $data['fname'] : "";
+        $this->lname = isset($data['lname']) ? $data['lname'] : "";
         $this->age = isset($data['age']) ? $data['age'] : "";
         $this->gender = isset($data['gender']) ? $data['gender'] : "";
-        $this->location = isset($data['location']) ? $data['location'] : "";
+        //$this->location = isset($data['location']) ? $data['location'] : "";
         $this->contactNumber = isset($data['contactNumber']) ? $data['contactNumber'] : "";
         $this->emailAddress = isset($data['emailAddress']) ? $data['emailAddress'] : "";
         $this->additionalInfo = isset($data['additionalInfo']) ? $data['additionalInfo'] : "";
     }
 
-    /*	@param array
-    *	Takes an array of info,
-    *	fills the post with the info
-    *	inserts the post into the post table.
-    */	
-	public function create(){
-		$db = new DB();
-		$db->connect();
+    /* @param array
+     * Takes an array of info,
+     * fills the post with the info
+     * inserts the post into the posts table.
+     */
+    public function create($newPost = false){
+        $db = new DB();
+        $db->connect();
         $data = array();
-        $data['user'] = "'$this->user'";
-        $data['firstName'] = "'$this->firstName'";
-        $data['lastName'] = "'$this->lastName'";
+        $data['userID'] = "'$this->userID'";
+        $data['fname'] = "'$this->fname'";
+        $data['lname'] = "'$this->lname'";
         $data['age'] = "'$this->age'";
         $data['gender'] = "'$this->gender'";
-        $data['location'] = "'$this->location'";
+        //$data['location'] = "'$this->location'";
         $data['contactNumber'] = "'$this->contactNumber'";
         $data['emailAddress'] = "'$this->emailAddress'";
-        $data['additionalinformation'] = "'$this->additionalInfo'";
-		$this->postid = $db->insert($data, "posts6");    
-
-		$db->closeCon();
-	}
-
-
-    /*	@param postid 		@return true
-    *	Scans the table for a post matching the post id
-    *	Return true if post is found
-    */	
-	public function fetchPost($postid){
-        $db = new DB();
-        $db->connect();
-        $result = $db->select("posts6", "postid = '$postid'", true);
-        $db->closeCon();
-        if ($result) {
-            return new Post($result);
+        $data['additionalinfo'] = "'$this->additionalInfo'";
+        if ($newPost) {
+            $data['date'] = "'".date("Y-m-d:h:i:s", time()). "'";
+            $this->postid = $db->insert($data, "Posts");
+        } else {
+            $db->update($data, "Posts", ' id = '.$this->postid);
         }
-        else
-            return false;
-    }
-
-    /*	@param postid 		@return true
-    *	Scans the table for a post matching the post id
-    *	Return true if post is found and deleted
-    */	
-    public function deletePost($postid){
-        $db = new DB();
-        $db->connect();
-        $result = $db->select("post", "postid = '$postid'", true);
         $db->closeCon();
-        if ($result) {
-        	delete("post", $postid);
-            return true;
-        }
-        else
-			return false;
+        return $this->postid;
     }
-
 
 }
-
 ?>
