1b520884fcb4cc5d1c4d80f670d0c3bad66f6fd0
Ho57 <ho57@purdue.edu>
2015-03-21 16:34:51 -0400

    Update search.php

--- search.php
+++ search.php
@@ -1,6 +1,40 @@
 <?php
+
 require_once "inc/global.inc.php";
+if (!isset($_SESSION['logged_in']))  {
+    header("Location: index.php");
+}
+
+$query = "";
+$method = "";
+
+
+if (isset($_POST['submit'])) {
+    $query = $_POST['query'];
+
+    $method = $_POST['method'];
+
+    //$user = unserialize($_SESSION['user']);
+    // error checking to be added!
+    $data = array();
+    $data['query'] = $query;
+    $data['method'] = $method;
+
+    //$data['userID'] = $user->id;
+
+
+    //$ph = new PostsHandler();
+
+    //$id = $ph->editPost($data);
+
+
+    //echo $id;
+    //if ($id) {
+    //    header("Location: post.php?id=$id");
+    //}
+}
 ?>
+
 
 <!doctype html>
 
@@ -9,7 +43,7 @@ require_once "inc/global.inc.php";
 
     <meta charset="utf-8">
     <title>
-        Search Posts | Mippsy
+        Search | Mippsy
     </title>
     <link rel="stylesheet" href="css/style.css" type="text/css" />
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
@@ -25,13 +59,29 @@ require_once "inc/global.inc.php";
         ?>
     </div>
     <div id="content">
+        <div id="searchpost">
         <br><br>
         <center>
-            <h3>This site is currently under construction!</h3>
-            <br>
-            <img src="images/underconstruction.jpg">
+            <h2>Search Post</h2>
+            <form action="searchpost.php" method="POST">
+                <table>
+                    <tr>
+                        <td>Search:</td>
+                        <td><input type="text" name="query"></td>
+                        <td>
+                            <select name="method">
+                                <option value="N">Search By Name</option>
+                                <option value="D">Search By Date</option>
+                                <option value="L">Search By Location</option>
+                            </select>
+                        </td>
+                    </tr>
+                </table>
+                <input type="submit" value="Submit" name="submit">
+            </form>
         </center>
         <br><br><br>
+        </div>
     </div>
     <div id="footer">
         <?php include "inc/footer.php"; ?>
